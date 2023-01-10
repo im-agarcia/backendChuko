5b7c73d135b6f739d299dba640fcca9b4315afe4
im-agarcia <im.agarcia@outlook.com>
2023-01-09 22:48:48 -0300

    command start added

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 const dotenv = require('dotenv').config()
 
 const db = mysql.createConnection({
-    host: process.env.HOST_NAME,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
